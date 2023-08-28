4f34172f6b96d6e8be3f58b3704d2994a631bf8a
Henrique Caúla <henrique.caula@vtex.com.br>
2023-08-28 15:05:07 -0300

    feat: add specific products for each account

--- node/clients/AdServer.ts
+++ node/clients/AdServer.ts
@@ -3,6 +3,11 @@ import { ExternalClient } from '@vtex/api'
 
 import type { AdServerRequest, AdServerResponse } from '../typings/AdServer'
 
+const accountToId: Record<string, string> = {
+  extrafarmav2: '53793',
+  storecomponents: '14',
+}
+
 class AdServer extends ExternalClient {
   public static BASE_URL = 'https://ad-server.vtex.systems'
 
@@ -13,12 +18,14 @@ class AdServer extends ExternalClient {
   public getSponsoredProducts(
     body: AdServerRequest
   ): Promise<AdServerResponse> {
+    const productId = accountToId[this.context.account]
+
     return Promise.resolve({
       adResponseId: '65daf770-e255-4ca1-b50e-5f24ee6a6d56',
       adRequestId: '07017094-3dfa-4bda-86b3-9f17550b289f',
       sponsoredProducts: [
         {
-          productId: '53793',
+          productId,
           campaignId: '3add0f42-3b09-4506-b86f-3df802486b1c',
           adId: '77a33755-08cb-40ff-b5e3-992eddd0c6fb',
           actionCost: 0.32,
