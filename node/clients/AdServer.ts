82033de180baedb28b88bce3011dd125f609edf2
Henrique Caúla <henrique.caula@vtex.com.br>
2023-12-04 15:41:35 -0300

    fix: remove mock from AdServer

--- node/clients/AdServer.ts
+++ node/clients/AdServer.ts
@@ -11,69 +11,12 @@ class AdServer extends ExternalClient {
   }
 
   public getSponsoredProducts(
-    _body: AdServerRequest
+    body: AdServerRequest
   ): Promise<AdServerResponse> {
-    const response: AdServerResponse = {
-      adRequestId: '123',
-      adResponseId: '321',
-      sponsoredProducts: [
-        {
-          productId: '33232',
-          actionCost: 0.5,
-          campaignId: '321',
-          adId: '123',
-        },
-        // {
-        //   productId: '2',
-        //   actionCost: 0.5,
-        //   campaignId: '321',
-        //   adId: '321',
-        // },
-        // {
-        //   productId: '48103',
-        //   actionCost: 0.5,
-        //   campaignId: '321',
-        //   adId: '123',
-        // },
-        // {
-        //   productId: '98698',
-        //   actionCost: 0.5,
-        //   campaignId: '321',
-        //   adId: '321',
-        // },
-        // {
-        //   productId: '44308',
-        //   actionCost: 0.5,
-        //   campaignId: '321',
-        //   adId: '456',
-        // },
-        // {
-        //   productId: '61312',
-        //   actionCost: 0.5,
-        //   campaignId: '321',
-        //   adId: '654',
-        // },
-        // {
-        //   productId: '51455',
-        //   actionCost: 0.5,
-        //   campaignId: '321',
-        //   adId: '789',
-        // },
-        // {
-        //   productId: '59369',
-        //   actionCost: 0.5,
-        //   campaignId: '321',
-        //   adId: '987',
-        // },
-      ],
-    }
-
-    return Promise.resolve(response)
-
-    // return this.http.post('/api/v1/sponsored-products', {
-    //   accountName: this.context.account,
-    //   ...body,
-    // })
+    return this.http.post('/api/v1/sponsored-products', {
+      accountName: this.context.account,
+      ...body,
+    })
   }
 }
 
