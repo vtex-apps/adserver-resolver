994a022c363b6a4f74c96fe72fa6977bb051662d
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2024-10-22 18:17:04 -0300

    feat: Add getSponsoredBanners method to AdServer

--- node/clients/AdServer.ts
+++ node/clients/AdServer.ts
@@ -1,7 +1,12 @@
 import type { InstanceOptions, IOContext } from '@vtex/api'
 import { ExternalClient } from '@vtex/api'
 
-import type { AdServerRequest, AdServerResponse } from '../typings/AdServer'
+import type {
+  AdServerRequest,
+  AdServerResponse,
+  AdServerSponsoredBannersRequest,
+  AdServerSponsoredBannersResponse,
+} from '../typings/AdServer'
 
 class AdServer extends ExternalClient {
   public static BASE_URL = 'https://ad-server.vtex.systems'
@@ -23,6 +28,20 @@ class AdServer extends ExternalClient {
     }
 
     return this.http.post('/api/v1/sponsored-products', {
+      accountName: this.context.account,
+      ...body,
+    })
+  }
+
+  public getSponsoredBanners(
+    body: AdServerSponsoredBannersRequest
+  ): Promise<AdServerSponsoredBannersResponse> {
+    if (process.env.VTEX_APP_LINK) {
+      // eslint-disable-next-line no-console
+      console.log(JSON.stringify(body, null, 2))
+    }
+
+    return this.http.post('/api/v1/ads/banner', {
       accountName: this.context.account,
       ...body,
     })
